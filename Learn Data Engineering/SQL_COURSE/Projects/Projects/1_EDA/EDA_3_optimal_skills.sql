11d11cfff53326fbd653d26aa698cc8778c658aa
panchalaman <panchalaman@hotmail.com>
2026-02-19 23:57:04 +0100

    19.02.2026

--- Learn Data Engineering/SQL_COURSE/Projects/Projects/1_EDA/EDA_3_optimal_skills.sql
+++ Learn Data Engineering/SQL_COURSE/Projects/Projects/1_EDA/EDA_3_optimal_skills.sql
@@ -9,24 +9,26 @@ Question: What are the most optimal skills for data engineers—balancing both d
 
 SELECT 
     sd.skills,
-    ROUND(MEDIAN(jpf.salary_year_avg), 1) AS median_salary,
-    COUNT(jpf.*) AS demand_count,
-    ROUND(LN(COUNT(jpf.*)), 1) AS ln_demand_count,
-    ROUND((LN(COUNT(jpf.*)) * MEDIAN(jpf.salary_year_avg))/1_000_000, 2) AS optimal_score
+    ROUND(MEDIAN(jpf.salary_year_avg), 0) AS median_salary,
+    COUNT(jpf.salary_year_avg) AS demand_count,
+    ROUND(LN(COUNT(jpf.*)), 0) AS ln_demand_count,
+    ROUND((MEDIAN(jpf.salary_year_avg) * LN(COUNT(jpf.*))/1000000), 2) AS optimal_score,
+    
 FROM job_postings_fact jpf
 INNER JOIN skills_job_dim sjd ON jpf.job_id = sjd.job_id
 INNER JOIN skills_dim sd ON sjd.skill_id = sd.skill_id
 WHERE
     jpf.job_title_short = 'Data Engineer'
-    AND jpf.salary_year_avg IS NOT NULL
     AND jpf.job_work_from_home = True 
+    AND jpf.salary_year_avg IS NOT NULL
 GROUP BY
     sd.skills
-HAVING 
-    COUNT(sjd.job_id) >= 100
+HAVING
+    COUNT(sd.skills) >= 100
 ORDER BY
     optimal_score DESC
 LIMIT 25;
+
 
 /*
 Here's a breakdown of the most optimal skills for Data Engineers, based on both high demand and high salaries:
@@ -57,31 +59,31 @@ Skills that consistently appear near the top balance a strong combination of mar
 │   skills   │ median_salary │ demand_count │ ln_demand_count │ optimal_score │
 │  varchar   │    double     │    int64     │     double      │    double     │
 ├────────────┼───────────────┼──────────────┼─────────────────┼───────────────┤
-│ terraform  │      184000.0 │          193 │             5.3 │          0.98 │
+│ terraform  │      184000.0 │          193 │             5.0 │          0.97 │
 │ python     │      135000.0 │         1133 │             7.0 │          0.95 │
-│ aws        │      137320.3 │          783 │             6.7 │          0.92 │
 │ sql        │      130000.0 │         1128 │             7.0 │          0.91 │
-│ airflow    │      150000.0 │          386 │             6.0 │           0.9 │
-│ spark      │      140000.0 │          503 │             6.2 │          0.87 │
-│ kafka      │      145000.0 │          292 │             5.7 │          0.83 │
-│ snowflake  │      135500.0 │          438 │             6.1 │          0.83 │
-│ azure      │      128000.0 │          475 │             6.2 │          0.79 │
-│ java       │      135000.0 │          303 │             5.7 │          0.77 │
-│ scala      │      137290.5 │          247 │             5.5 │          0.76 │
+│ aws        │      137320.0 │          783 │             7.0 │          0.91 │
+│ airflow    │      150000.0 │          386 │             6.0 │          0.89 │
+│ spark      │      140000.0 │          503 │             6.0 │          0.87 │
+│ kafka      │      145000.0 │          292 │             6.0 │          0.82 │
+│ snowflake  │      135500.0 │          438 │             6.0 │          0.82 │
+│ azure      │      128000.0 │          475 │             6.0 │          0.79 │
+│ java       │      135000.0 │          303 │             6.0 │          0.77 │
+│ scala      │      137290.0 │          247 │             6.0 │          0.76 │
+│ git        │      140000.0 │          208 │             5.0 │          0.75 │
 │ kubernetes │      150500.0 │          147 │             5.0 │          0.75 │
-│ databricks │      132750.0 │          266 │             5.6 │          0.74 │
-│ git        │      140000.0 │          208 │             5.3 │          0.74 │
-│ redshift   │      130000.0 │          274 │             5.6 │          0.73 │
-│ gcp        │      136000.0 │          196 │             5.3 │          0.72 │
-│ hadoop     │      135000.0 │          198 │             5.3 │          0.72 │
-│ nosql      │      134415.0 │          193 │             5.3 │          0.71 │
+│ databricks │      132750.0 │          266 │             6.0 │          0.74 │
+│ redshift   │      130000.0 │          274 │             6.0 │          0.73 │
+│ gcp        │      136000.0 │          196 │             5.0 │          0.72 │
+│ hadoop     │      135000.0 │          198 │             5.0 │          0.71 │
+│ nosql      │      134415.0 │          193 │             5.0 │          0.71 │
 │ pyspark    │      140000.0 │          152 │             5.0 │           0.7 │
-│ docker     │      135000.0 │          144 │             5.0 │          0.68 │
-│ mongodb    │      135750.0 │          136 │             4.9 │          0.67 │
-│ r          │      134775.0 │          133 │             4.9 │          0.66 │
-│ go         │      140000.0 │          113 │             4.7 │          0.66 │
-│ github     │      135000.0 │          127 │             4.8 │          0.65 │
-│ bigquery   │      135000.0 │          123 │             4.8 │          0.65 │
+│ docker     │      135000.0 │          144 │             5.0 │          0.67 │
+│ mongodb    │      135750.0 │          136 │             5.0 │          0.67 │
+│ r          │      134775.0 │          133 │             5.0 │          0.66 │
+│ go         │      140000.0 │          113 │             5.0 │          0.66 │
+│ bigquery   │      135000.0 │          123 │             5.0 │          0.65 │
+│ github     │      135000.0 │          127 │             5.0 │          0.65 │
 ├────────────┴───────────────┴──────────────┴─────────────────┴───────────────┤
 │ 25 rows                                                           5 columns │
 └─────────────────────────────────────────────────────────────────────────────┘
